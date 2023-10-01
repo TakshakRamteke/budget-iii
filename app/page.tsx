1894be2990fcd7cd42fe66f2c3da337dceb97705
TakshakRamteke <takshakramteke0708@gmail.com>
2023-10-02 03:05:05 +0530

    FEAT : Integrated all things into one page

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,102 @@
-import moment from 'moment';
+import Card from '@/components/card';
+import ExpensesTable from '@/components/expensesTable';
+import IncomesTable from '@/components/incomesTable';
 
 export default async function Home() {
     const expensesResponse = await fetch('http://localhost:3000/api/expenses', {
-        next: { revalidate: 10 },
+        next: { revalidate: 5 },
     });
-    const expenses = await expensesResponse.json();
+    const expenses: Expense[] = await expensesResponse.json();
 
-    function addBackground(color: string) {
-        switch (color) {
-            case 'yellow':
-                return 'bg-yellow-300';
-            case 'pink':
-                return 'bg-pink-300';
-            case 'red':
-                return 'bg-red-300';
-            case 'purple':
-                return 'bg-purple-300';
-            case 'gray':
-                return 'bg-gray-300';
-            case 'orange':
-                return 'bg-orange-300';
-            default:
-                return '';
-        }
-    }
+    const incomesResponse = await fetch('http://localhost:3000/api/incomes', {
+        next: { revalidate: 5 },
+    });
+    const incomes: Income[] = await incomesResponse.json();
 
-    const allExpensesArray = expenses.map((expense) =>
-        parseInt(expense.properties.amount.number),
+    const totalIncomesAmount = incomes
+        .map((income) => income.properties.amount.number)
+        .reduce((current, next) => {
+            return current + next;
+        });
+
+    const allExpensesAmountsArray = expenses.map(
+        (expense) => expense.properties.amount.number,
     );
 
-    const totalExpenses = allExpensesArray.reduce((current, next) => {
-        return current + next;
-    }, 0);
+    const totalExpensesAmount = allExpensesAmountsArray.reduce(
+        (current: number, next: number) => {
+            return current + next;
+        },
+        0,
+    );
+
+    const allCategouriesArray: string[] = [];
+    expenses.map((expense) => {
+        if (
+            !allCategouriesArray.includes(
+                expense.properties.category.select.name,
+            )
+        ) {
+            allCategouriesArray.push(expense.properties.category.select.name);
+        }
+    });
 
     return (
         <>
-            <p>Expenses : </p>
-            <table className='table-auto divide-y w-1/2'>
-                <thead className='text-xs uppercase text-gray-500 mt-0'>
-                    <tr>
-                        <th className='px-2 first:pl-5 last:pr-5 py-3'>
-                            Title
-                        </th>
-                        <th className='px-2 first:pl-5 last:pr-5 py-3 text-right'>
-                            Amount
-                        </th>
-                        <th className='px-2 first:pl-5 last:pr-5 py-3 text-right'>
-                            Category
-                        </th>
-                        <th className='px-2 first:pl-5 last:pr-5 py-3'>Date</th>
-                    </tr>
-                </thead>
-                <tbody className='text-md divide-y'>
-                    {expenses.map((expense) => (
-                        <tr key={expense.id}>
-                            <td className='pl-5 p-2 whitespace-nowrap'>
-                                {
-                                    expense.properties.expense.title[0].text
-                                        .content
-                                }
-                            </td>
-                            <td className='p-2 whitespace-nowrap text-right'>
-                                ₹{' '}
-                                {expense.properties.amount.number.toLocaleString(
-                                    'en-US',
-                                )}
-                            </td>
-                            <td
-                                className={`p-2 whitespace-now text-right flex`}
-                            >
-                                <p
-                                    className={`rounded-sm px-1 ml-auto ${addBackground(
-                                        expense.properties.category.select
-                                            .color,
-                                    )}`}
-                                >
-                                    {expense.properties.category.select.name}
-                                </p>
-                            </td>
-                            <td className='p-2 whitespace-nowrap text-center'>
-                                {moment(
-                                    expense.properties.date.date.start,
-                                ).format('MMMM Do, YYYY h:mm a')}
-                            </td>
-                        </tr>
-                    ))}
-                    <tr>
-                        <td className='pl-5 p-2 whitespace-nowrap'>Total </td>
-                        <td className='p-2 whitespace-nowrap text-right'>
-                            ₹ {totalExpenses.toLocaleString('en-US')}
-                        </td>
-                        <td
-                            className={`p-2 whitespace-now text-right flex`}
-                        ></td>
-                        <td className='p-2 whitespace-nowrap text-center'></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-3'>
+                <div className='p-4 min-w-[13rem] border border-slate-200 shrink-0 rounded shadow-sm bg-white flex flex-row items-center'>
+                    <p className='text-lg font-medium mt-auto'>Total Incomes</p>
+                    <p className='font-semibold text-3xl ml-auto mt-0'>
+                        ₹ {totalIncomesAmount.toLocaleString()}
+                    </p>
+                </div>
+
+                <div className='p-4 min-w-[13rem] border border-slate-200 shrink-0 rounded shadow-sm bg-white flex flex-row items-center'>
+                    <p className='text-lg font-medium mt-auto'>
+                        Total Expenses
+                    </p>
+                    <p className='font-semibold text-3xl ml-auto mt-0'>
+                        ₹ {totalExpensesAmount.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+
+            <div className='flex overflow-x-scroll my-3'>
+                {allCategouriesArray.map((category) => (
+                    <Card
+                        key={Math.random() * 10}
+                        title={`${category} Expense`}
+                        amount={expenses
+                            .filter(
+                                (expense) =>
+                                    expense.properties.category.select.name ===
+                                    category,
+                            )
+                            .map((expense) => expense.properties.amount.number)
+                            .reduce((current, next) => {
+                                return current + next;
+                            })}
+                    />
+                ))}
+            </div>
+
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
+                <div>
+                    <p className='text-2xl font-semibold my-3'>Incomes : </p>
+                    <IncomesTable
+                        totalAmount={totalIncomesAmount}
+                        capitalData={incomes}
+                    />
+                </div>
+
+                <div>
+                    <p className='text-2xl font-semibold my-3'>Expenses : </p>
+                    <ExpensesTable
+                        totalAmount={totalExpensesAmount}
+                        capitalData={expenses}
+                    />
+                </div>
+            </div>
         </>
     );
 }
