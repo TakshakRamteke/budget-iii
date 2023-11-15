a0ac76489b90f7136f221e26da0bac1acd40f894
TakshakRamteke <takshakramteke0708@gmail.com>
2023-11-15 22:05:48 +0530

    FEAT : Added date provider for filtering based on current month

---
+++ utils/DateProvider.tsx
@@ -0,0 +1,48 @@
+import { createContext, useState } from 'react';
+import moment from 'moment';
+
+export const datesContext = createContext<any | null>(null);
+
+export default function DatesProvider({
+    children,
+}: {
+    children: JSX.Element | JSX.Element[];
+}) {
+    const currentDate = new Date();
+    const [month, setMonth] = useState(() => {
+        if (currentDate.getDate() >= 7) {
+            return currentDate.getMonth() + 1;
+        } else {
+            return currentDate.getMonth();
+        }
+    });
+
+    const [fromDate, setFromDate] = useState(
+        new Date(
+            //@ts-ignore
+            moment(`07/${month}/2023,00:00:00`, 'DD/MM/YYYY,hh:mm:ss'),
+        ),
+    );
+
+    const [toDate, setToDate] = useState(
+        new Date(
+            //@ts-ignore
+            moment(`07/${month + 1}/2023,00:00:00`, 'DD/MM/YYYY,hh:mm:ss'),
+        ),
+    );
+
+    return (
+        <datesContext.Provider
+            value={{
+                month,
+                fromDate,
+                toDate,
+                setFromDate,
+                setToDate,
+                setMonth,
+            }}
+        >
+            {children}
+        </datesContext.Provider>
+    );
+}
