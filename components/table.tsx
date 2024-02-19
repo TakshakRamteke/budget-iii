05e32fa775edf18516b44867ce3e5dbe83f99358
TakshakRamteke <takshakramteke0708@gmail.com>
2024-02-19 14:00:00 +0100

    FIX : Ellipses for expense title in table

--- components/table.tsx
+++ components/table.tsx
@@ -65,13 +65,15 @@ export default function Table({
                         <>
                             <View className='flex flex-row items-center border-slate-600'>
                                 <Text
-                                    className={`text-white w-48 border-x border-b border-slate-600 p-2 decoration-slate-300 ${record.index === 0
+                                    className={`text-white w-48 border-x border-b border-slate-600 p-2 decoration-slate-300 ${
+                                        record.index === 0
                                             ? 'border-t bg-[#1C1C1C] rounded-tl'
                                             : 'underline'
-                                        } ${record.index === endIndex
+                                    } ${
+                                        record.index === endIndex
                                             ? 'rounded-bl'
                                             : ''
-                                        }`}
+                                    }`}
                                     style={{ textDecorationColor: 'gray' }}
                                 >
                                     {
@@ -85,7 +87,12 @@ export default function Table({
                                                     },
                                                 }}
                                             >
-                                                {record.item.name}
+                                                {record.item.name.length <= 20
+                                                    ? record.item.name
+                                                    : record.item.name.slice(
+                                                          0,
+                                                          20,
+                                                      ) + '...'}
                                             </Link>
                                         ) : (
                                             `${record.item.name}`
@@ -93,41 +100,45 @@ export default function Table({
                                     }
                                 </Text>
                                 <Text
-                                    className={`text-white w-24 text-right border-r border-b border-slate-600 p-2 px-3 ${record.index === 0
+                                    className={`text-white w-24 text-right border-r border-b border-slate-600 p-2 px-3 ${
+                                        record.index === 0
                                             ? 'border-t bg-[#1C1C1C]'
                                             : ''
-                                        }`}
+                                    }`}
                                 >
                                     ₹ {record.item.amount.toLocaleString()}
                                 </Text>
                                 <Text
-                                    className={`text-white w-32 text-center border-r border-b border-slate-600 p-2 capitalize ${record.index === 0
+                                    className={`text-white w-32 text-center border-r border-b border-slate-600 p-2 capitalize ${
+                                        record.index === 0
                                             ? 'border-t bg-[#1C1C1C]'
                                             : ''
-                                        }`}
+                                    }`}
                                 >
                                     {record.item.category}
                                 </Text>
                                 <Text
-                                    className={`text-white w-48 text-right border-r border-b border-slate-600 p-2 ${record.index === 0
+                                    className={`text-white w-48 text-right border-r border-b border-slate-600 p-2 ${
+                                        record.index === 0
                                             ? 'border-t bg-[#1C1C1C] rounded-tr'
                                             : ''
-                                        } ${record.index === endIndex
+                                    } ${
+                                        record.index === endIndex
                                             ? 'rounded-br'
                                             : ''
-                                        }`}
+                                    }`}
                                 >
                                     {record.index === 0
                                         ? record.item.date
                                         : moment(
-                                            record.item.date,
-                                            'DD/MM/YYYY',
-                                        ).format('Do MMM YYYY') +
-                                        ', ' +
-                                        moment(
-                                            record.item.time,
-                                            'hh:mm:ss',
-                                        ).format('hh:mm a')}
+                                              record.item.date,
+                                              'DD/MM/YYYY',
+                                          ).format('Do MMM YYYY') +
+                                          ', ' +
+                                          moment(
+                                              record.item.time,
+                                              'hh:mm:ss',
+                                          ).format('hh:mm a')}
                                 </Text>
                             </View>
                         </>
@@ -137,8 +148,9 @@ export default function Table({
             {paginate && hasToBePaginated ? (
                 <View className='flex flex-row my-2 ml-auto border border-slate-600 rounded'>
                     <Pressable
-                        className={`p-1 ${canGoBackward ? 'bg-[#1C1C1C]' : 'bg-[#161616]'
-                            } rounded-sm px-4 border-r border-slate-600`}
+                        className={`p-1 ${
+                            canGoBackward ? 'bg-[#1C1C1C]' : 'bg-[#161616]'
+                        } rounded-sm px-4 border-r border-slate-600`}
                         onPress={() => {
                             if (canGoBackward) {
                                 setEndIndex(startIndex);
@@ -147,15 +159,17 @@ export default function Table({
                         }}
                     >
                         <Text
-                            className={`${canGoBackward ? 'text-white' : 'text-slate-600'
-                                }`}
+                            className={`${
+                                canGoBackward ? 'text-white' : 'text-slate-600'
+                            }`}
                         >
                             Prev
                         </Text>
                     </Pressable>
                     <Pressable
-                        className={`p-1 ${canGoForward ? 'bg-[#1C1C1C]' : 'bg-[#161616]'
-                            } rounded-sm px-4`}
+                        className={`p-1 ${
+                            canGoForward ? 'bg-[#1C1C1C]' : 'bg-[#161616]'
+                        } rounded-sm px-4`}
                         onPress={() => {
                             if (canGoForward) {
                                 setStartIndex(endIndex);
@@ -164,8 +178,9 @@ export default function Table({
                         }}
                     >
                         <Text
-                            className={`${canGoForward ? 'text-white' : 'text-slate-600'
-                                }`}
+                            className={`${
+                                canGoForward ? 'text-white' : 'text-slate-600'
+                            }`}
                         >
                             Next
                         </Text>
